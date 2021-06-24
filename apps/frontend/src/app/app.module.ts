047fcc4d1b6fce3745c852bfddb36f6c5dd4d774
I-MediaSolutions info@i-mediasolutions.nl <info@i-mediasolutions.nl>
2021-06-24 20:20:29 +0200

    initial playground with smart/dumb components

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PasswordComponent } from './password/password.component';
+import { FindComponent } from './find/find.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PasswordComponent, FindComponent, SharedComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
