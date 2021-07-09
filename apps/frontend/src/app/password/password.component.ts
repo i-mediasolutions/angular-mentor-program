1bbc9fdbecdf35d1b7fb155a366e53fc0d71f0fe
Vlasis Charalampous <vlasischar@gmail.com>
2021-07-09 18:17:13 +0300

    Revert "minor fix"
    This reverts commit 6d36afe625e6a532fe71ef06ac15d8048d463335.

--- apps/frontend/src/app/password/password.component.ts
+++ apps/frontend/src/app/password/password.component.ts
@@ -1,18 +1,18 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { SharedViewModel } from '../components/shared/shared.models';
+import { Component, ViewEncapsulation } from "@angular/core";
+import { SharedViewModel } from "../components/shared/shared.models";
 
 @Component({
-  selector: 'angular-mentor-program-password',
-  templateUrl: './password.component.html',
-  styleUrls: ['./password.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  selector: "angular-mentor-program-password",
+  templateUrl: "./password.component.html",
+  styleUrls: ["./password.component.css"],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class PasswordComponent {
   vm: SharedViewModel = {
-    title: 'Lost password component',
-    text: 'from password component',
-    btnCaption: 'get password',
-    placeholder: 'username or e-mail address'
+    title: "Lost password component",
+    text: "from password component",
+    btnCaption: "get password",
+    placeholder: "username or e-mail address"
   };
 
   onBtnClick(value: string) {
